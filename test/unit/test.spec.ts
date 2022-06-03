f3cd8fec1e50bfa897e91a862bb266e905cda4ab
Tomaž Bratanič <tomaz.bratanic@gmail.com>
2022-06-03 08:38:38 +0200

    fact(server test placeholder) Add server test placeholder

---
+++ test/unit/test.spec.ts
@@ -0,0 +1,15 @@
+describe("Testing API routes", function() {
+
+    describe("Example unit test for a route", function() {
+
+    });
+
+    describe("Example unit test for a controller", function() {
+
+    });
+
+    describe("Example integaration test", function() {
+
+    });
+
+});
